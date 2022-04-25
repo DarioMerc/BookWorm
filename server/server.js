0d235b4419ae18d54e7b76bddd9db120788bf40d
Dario Mercuri <dario.mercuri31@gmail.com>
2022-04-25 17:34:25 -0400

    build (again)

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 const PORT = 8000;
 
 const {
